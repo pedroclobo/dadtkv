d3be961ab18625e39af9df64a230d8239fb066f5
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-10-21 08:25:49 +0100

    feat: add time slot timer to TM

--- TransactionManager/Program.cs
+++ TransactionManager/Program.cs
@@ -18,14 +18,14 @@ public class TransactionManager
             string identifier = args[0];
             string filename = args[1];
 
-            ConfigurationParser configurationParser = ConfigurationParser.From(filename);
-            string host = configurationParser.ServerHost(identifier);
-            int port = configurationParser.ServerPort(identifier);
+            ConfigurationParser parser = ConfigurationParser.From(filename);
+            string host = parser.ServerHost(identifier);
+            int port = parser.ServerPort(identifier);
 
-            Dictionary<string, Uri> transactionManagerURLS = configurationParser.TransactionManagerUrls();
+            Dictionary<string, Uri> transactionManagerURLS = parser.TransactionManagerUrls();
             transactionManagerURLS.Remove(identifier); // Remove own URL
 
-            Dictionary<string, Uri> leaseManagerURLS = configurationParser.LeaseManagerUrls();
+            Dictionary<string, Uri> leaseManagerURLS = parser.LeaseManagerUrls();
 
             // Create server
             State state = new State();
@@ -39,22 +39,53 @@ public class TransactionManager
                 Services = {
                     DADTKVClientService.BindService(new DADTKVClientServiceImpl(identifier, state, urbFrontend, leaseFrontend, leaseManagementFrontend, leaseQueue)),
                     LeaseManagementService.BindService(new LeaseManagementServiceImpl(identifier, leaseQueue)),
-                    PaxosLearnerService.BindService(new PaxosLearnerServiceImpl(configurationParser.LeaseManagerIdentifiers().Count(), leaseQueue)),
+                    PaxosLearnerService.BindService(new PaxosLearnerServiceImpl(parser.LeaseManagerIdentifiers().Count(), leaseQueue)),
                     URBService.BindService(new URBServiceImpl(identifier, state))
                 },
                 Ports = { new Grpc.Core.ServerPort(host, port, Grpc.Core.ServerCredentials.Insecure) }
             };
 
             Console.WriteLine($"Transaction Manager {identifier} will be listening on host {host} and port {port}");
-            Console.WriteLine($"Starting at: {configurationParser.WallTime}");
+            Console.WriteLine($"Starting at: {parser.WallTime}");
 
             // Configure HTTP for client connections in Register method
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
             // Wait for wall time
-            await configurationParser.WaitForWallTimeAsync();
+            await parser.WaitForWallTimeAsync();
 
             server.Start();
+
+            int currentTimeSlot = 1;
+            int timeSlots = parser.TimeSlots;
+            TimeSpan slotDuration = parser.SlotDuration;
+            parser.Update(currentTimeSlot);
+
+            Timer timer = null;
+            timer = new Timer(async _ =>
+            {
+                if (currentTimeSlot > timeSlots)
+                {
+                    Console.WriteLine("Time slots expired! Shutting down!");
+                    Console.WriteLine("Press any key to exit...");
+
+                    urbFrontend.Shutdown();
+                    leaseFrontend.Shutdown();
+                    leaseManagementFrontend.Shutdown();
+                    server.ShutdownAsync().Wait();
+
+                    timer.Dispose();
+
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+                parser.Update(currentTimeSlot);
+
+                Console.WriteLine($"\nTime Slot: {currentTimeSlot}");
+
+                currentTimeSlot++;
+            }, null, TimeSpan.Zero, slotDuration);
 
             Console.WriteLine("Press any key to exit...");
             Console.WriteLine();
